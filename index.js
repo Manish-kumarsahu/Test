b1886600ab0a428c9cdbd776d45172178e354ed5
manish sahu <sahum6113007@gmail.coom>
2026-02-02 20:47:05 +0530

    learnig express

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Application is working on ${port}`);
+});
+
+// app.use("/apple",(req,res)=>{
+//     res.send({
+//         Name:"apple",
+//         color:"red",
+//     });
+// });
+
+// app.use("/Fruits", (req, res) => {
+//   let htmlstr = ` <ul>
+//         <li>Apple</li>
+//         <li>Orange</li>
+//     </ul>`;
+//   res.send(htmlstr);
+// });
+
+// app.get("/:username",(req,res)=>{
+//     let username=req.params.username;
+//     let htmlstr=`<h1> Welcome ${username}`;
+//     res.send(htmlstr);
+// });
