9c89833b3cea16c385d58c2d44dbd0064e366a82
manish sahu <sahum6113007@gmail.coom>
2026-02-02 21:06:00 +0530

    update

--- index.js
+++ index.js
@@ -6,23 +6,23 @@ app.listen(port, () => {
   console.log(`Application is working on ${port}`);
 });
 
-// app.use("/apple",(req,res)=>{
-//     res.send({
-//         Name:"apple",
-//         color:"red",
-//     });
-// });
+app.use("/apple",(req,res)=>{
+    res.send({
+        Name:"apple",
+        color:"red",
+    });
+});
 
-// app.use("/Fruits", (req, res) => {
-//   let htmlstr = ` <ul>
-//         <li>Apple</li>
-//         <li>Orange</li>
-//     </ul>`;
-//   res.send(htmlstr);
-// });
+app.use("/Fruits", (req, res) => {
+  let htmlstr = ` <ul>
+        <li>Apple</li>
+        <li>Orange</li>
+    </ul>`;
+  res.send(htmlstr);
+});
 
-// app.get("/:username",(req,res)=>{
-//     let username=req.params.username;
-//     let htmlstr=`<h1> Welcome ${username}`;
-//     res.send(htmlstr);
-// });
+app.get("/:username",(req,res)=>{
+    let username=req.params.username;
+    let htmlstr=`<h1> Welcome ${username}`;
+    res.send(htmlstr);
+});
